438f64b75135b75aa1df0f15f47dd268ee35f122
tksheppard <tksheppard@nvisionative.com>
2021-12-23 10:54:52 -0500

    Initial Development

--- partials/_footer.ascx
+++ partials/_footer.ascx
@@ -1,6 +1,12 @@
 <footer>
   <div class="container py-2">
     <div class="row justify-content-center">
+      <div class="col-12 mb-1">
+        <nav>
+          <dnn:MENU id="footermenu" MenuStyle="menus/footer" runat="server" NodeSelector="*"></dnn:MENU>
+        </nav>
+      </div>
+      <div class="w-100"></div>
       <div class="col-auto">
         <dnn:COPYRIGHT id="dnnCopyright" runat="server" />
       </div>
--- src/js/custom.js
+++ src/js/custom.js
@@ -10,4 +10,21 @@ jQuery( document ).ready(function($) {
     expandIcon: '',
     collapseIcon: ''
   });
+
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+    var target = $(this.hash);
+    $('html,body').stop().animate({
+      scrollTop: target.offset().top - 150
+    }, 'linear');
+    $('.collapse-button').removeClass('expanded').children('i').removeClass('fa-times').addClass('fa-bars');
+    $('.slimmenu.collapsed').hide();   
+  });    
+  if (location.hash){
+    var id = $(location.hash);
+  }
+  $(window).load(function() {
+    if (location.hash){
+      $('html,body').animate({scrollTop: id.offset().top -150}, 'linear');
+    }
+  });
 });
